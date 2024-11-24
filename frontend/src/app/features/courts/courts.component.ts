f177e0f1128d521a67eade82b94e2c6001de7319
Mikołaj Petecki <mikolajpe2@gmail.com>
2024-11-24 23:40:23 +0100

    basic features init

---
+++ frontend/src/app/features/courts/courts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courts',
+  standalone: true,
+  imports: [],
+  templateUrl: './courts.component.html',
+  styleUrl: './courts.component.scss'
+})
+export class CourtsComponent {
+
+}
---
+++ frontend/src/app/features/tournaments/tournaments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tournaments',
+  standalone: true,
+  imports: [],
+  templateUrl: './tournaments.component.html',
+  styleUrl: './tournaments.component.scss'
+})
+export class TournamentsComponent {
+
+}
