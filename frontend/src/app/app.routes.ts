a0f2aba24eba06cbf154b8e74fe9aded4a8541c6
Mikołaj Petecki <mikolajpe2@gmail.com>
2025-01-07 18:32:30 +0100

    guards added; logout feature added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,18 +2,23 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
 import { RegisterComponent } from './features/register/register.component';
 import { LayoutComponent } from './features/layout/layout.component';
+import { AuthGuard } from './core/guards/auth.guard';
+import { ReverseAuthGuard } from './core/guards/reverse-auth.guard';
 
 export const routes: Routes = [
     {
         path: '',
         component: LayoutComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'login',
-        component: LoginComponent
+        component: LoginComponent,
+        canActivate: [ReverseAuthGuard]
     },
     {
         path: 'register',
-        component: RegisterComponent
+        component: RegisterComponent,
+        canActivate: [ReverseAuthGuard]
     },
 ];
--- frontend/src/app/features/layout/layout.component.ts
+++ frontend/src/app/features/layout/layout.component.ts
@@ -3,6 +3,9 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { CourtsComponent } from "../courts/courts.component";
 import { TournamentsComponent } from "../tournaments/tournaments.component";
+import { AuthService } from "../../core/services/auth.service";
+import { Router } from "@angular/router";
+
 
 @Component({
 	selector: "layout-register",
@@ -14,7 +17,14 @@ import { TournamentsComponent } from "../tournaments/tournaments.component";
 export class LayoutComponent {
 	selectedView: 'courts' | 'tournaments' = 'courts';
 
+	constructor(private authService: AuthService, private router: Router) {}
+
 	protected selectView(view: 'courts' | 'tournaments') {
 		this.selectedView = view;
+	}
+
+	protected logout() {
+		this.authService.removeToken();
+    	this.router.navigate(['/login']);
 	}
 }
